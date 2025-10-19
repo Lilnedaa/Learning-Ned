00a01cba37abd14026f08232ccc97f43415cbd4e
Nedaa <nedaatlou@gmail.com>
2025-10-19 09:13:42 +0300

    Initial Commit

---
+++ Learning Ned/Learning_NedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_NedApp.swift
+//  Learning Ned
+//
+//  Created by Nedaa on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct Learning_NedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
